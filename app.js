633fbe16dcce0f685f5042b03140838a253a0888
m6english <104322150+m6english@users.noreply.github.com>
2023-01-30 08:25:05 -0500

    added js functionality

--- app.js
+++ app.js
@@ -5,21 +5,30 @@ const reps = document.getElementById('reps');
 const exercise = document.getElementById('exercise');
 const btnSubmit = document.getElementById('btn-submit');
 const btnAdditional = document.getElementById('btn-additional');
-const chartGroup = document.getElementById('chart-group');
-const chartEx = document.getElementById('chart-ex');
-const chartMember = document.getElementById('chart-member');
-const memberArr = [
-    "Ben", "Clark", "Matt", "Mike", "Zach"
-];
-btnSubmit.addEventListener('click', () => {
-    chartGroup.innerHTML = group.value;
-    chartEx.innerHTML = `${sets.value} x ${reps.value} - ${exercise.value}`;
-    member.style.display = 'none';
-    group.style.display = 'none';
-    btnSubmit.style.display = 'none';   
-    btnAdditional.style.display = 'block';
-});
+const chartGroup = document.getElementsByClassName('chart-group');
+const chartEx = document.getElementsByClassName('chart-ex');
+const chartMember = document.getElementsByClassName('chart-member');
 
-btnAdditional.addEventListener('click', () => {
-    chartEx.innerHTML += `<br>${sets.value} x ${reps.value} - ${exercise.value}`;
+function clearValue() {
+    sets.value = '';
+    reps.value = '';
+    exercise.value = '';
+}
+
+btnSubmit.addEventListener('click', () => {
+    for ( let i = 0; i < chartMember.length; i++ ) {
+        if ( chartMember[i].textContent === member.value ) {
+            chartGroup[i].innerHTML = group.value;
+            chartEx[i].innerHTML = `${sets.value} x ${reps.value} - ${exercise.value}`;
+            member.style.display = 'none';
+            group.style.display = 'none';
+            btnSubmit.style.display = 'none';   
+            btnAdditional.style.display = 'block';
+            clearValue();
+            btnAdditional.addEventListener('click', () => {
+                chartEx[i].innerHTML += `<br>${sets.value} x ${reps.value} - ${exercise.value}`;
+                clearValue();
+            })
+        }
+    }
 });
